e8a422481a74e0f8a73c7b0e2bcccd44b880d9de
Ali <ali@alighali.com>
2017-11-19 16:10:20 +0100

    add animation to the about page

--- script/websiteHandler.js
+++ script/websiteHandler.js
@@ -1,5 +1,19 @@
     
-    $(function(){
+   var backgroundImages = { mainPage: '',
+                            aboutMe:'aboutme-background.jpg',
+                            training: '',
+                            holistingLiving: ''};
+
+   $(function(){
+       $(document).scroll( function(){
+           var imgOne = $('#img1').offset().top - $('#img1').height();
+                      console.log("elememnt offset is "+imgOne);
+                      console.log("window offset is "+$(window).scrollTop());
+                      if(imgOne == $(window).scrollTop()){
+                          $('#img1').css('left','400px');
+                      }
+                      
+       })
     $(document).on('click', 'a[href^="#"]', function (event) {
         event.preventDefault();
         $('html, body').animate({
@@ -8,3 +22,9 @@
     });
 })
 
+$(function(){
+    $('.hb__body__page__mainImage').on('click', function(){
+        console.log("url(./img/" + backgroundImages.aboutMe+ ")" );
+        $(this).attr('src',"url(./img/" + backgroundImages.aboutMe+ ")" );
+    });
+});
