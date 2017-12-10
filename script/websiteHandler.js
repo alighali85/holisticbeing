9f06634ffc6fe60bb860941b258b54e5f41ed499
Ali <ali@alighali.com>
2017-12-10 17:01:40 +0100

    add new page holisting being

--- script/websiteHandler.js
+++ script/websiteHandler.js
@@ -36,7 +36,7 @@
     $(document).on('click', 'a[href^="#"]', function (event) {
         event.preventDefault();
         $('html, body').animate({
-            scrollTop: $($.attr(this,'href')).offset().top
+            scrollTop: $($.attr(this,'href')).offset().top -100
         },500);
     });
 
