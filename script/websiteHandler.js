a42c881ef306896eeec8e105d1df4ba548e5001b
Ali <ali@alighali.com>
2017-12-26 13:10:52 +0100

    add contact page

--- script/websiteHandler.js
+++ script/websiteHandler.js
@@ -54,3 +54,12 @@
         var fullpageLink = pagesLinks[pageName];
         console.log(pageName) ;
     }
+// google maps//
+function myMap() {
+    var mapOptions = {
+        center: new google.maps.LatLng(51.5, -0.12),
+        zoom: 10,
+        mapTypeId: google.maps.MapTypeId.HYBRID
+    }
+var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+}
